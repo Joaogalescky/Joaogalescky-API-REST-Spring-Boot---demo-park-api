b98a994ff6a84e8b389ab56d2879973adbf0d2d6
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-29 13:32:44 -0300

    feat: Recurso para criar um novo usuário

--- src/main/java/com/jgalescky/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/jgalescky/demoparkapi/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.jgalescky.demoparkapi.service;
 
+import com.jgalescky.demoparkapi.entity.Usuario;
 import com.jgalescky.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
