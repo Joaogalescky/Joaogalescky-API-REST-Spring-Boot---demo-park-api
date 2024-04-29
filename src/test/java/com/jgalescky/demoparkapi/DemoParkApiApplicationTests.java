bad7a2eabd3a3e64a344e9a94f91c14b72699f7d
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-29 12:17:57 -0300

    "Primeiro commit - Gerando o projeto com Spring Initializer"

---
+++ src/test/java/com/jgalescky/demoparkapi/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgalescky.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
