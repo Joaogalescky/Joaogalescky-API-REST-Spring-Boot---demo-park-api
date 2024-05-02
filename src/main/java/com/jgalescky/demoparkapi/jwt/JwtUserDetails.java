56339f80e66fe175da03a649acde799c5ffe9499
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-02 16:55:31 -0300

    feat: Criando a classe JwtUserDetails

--- src/main/java/com/jgalescky/demoparkapi/jwt/JwtUserDetails.java
+++ src/main/java/com/jgalescky/demoparkapi/jwt/JwtUserDetails.java
@@ -1,2 +1,24 @@
-package com.jgalescky.demoparkapi.jwt;public class JwtUserDetails {
+package com.jgalescky.demoparkapi.jwt;
+
+import com.jgalescky.demoparkapi.entity.Usuario;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
 }
+
