3406198ad6f98f204932742d42c6f8dd2c9d9c55
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-02 10:13:30 -0300

    feat: Configurando o ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO USUARIOS (id, username, password, role) VALUES
+(100, 'ana@gmail.com', '123456', 'ROLE_ADMIN'),
+-- ou INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'bia@gmail.com', '123456', 'ROLE_CLIENTE'),
+-- ou INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'bob@gmail.com', '123456', 'ROLE_CLIENTE'),
+(101, 'bia@gmail.com', '123456', 'ROLE_CLIENTE'),
+(102, 'bob@gmail.com', '123456', 'ROLE_CLIENTE');
