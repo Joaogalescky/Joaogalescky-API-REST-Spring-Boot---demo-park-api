dddc7af9784a369b682823a9e484e69baa1f2593
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-02 13:29:44 -0300

    fix: de 'gmail' para 'email' na inserção doampo username

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO USUARIOS (id, username, password, role) VALUES
-(100, 'ana@gmail.com', '123456', 'ROLE_ADMIN'),
-(101, 'bia@gmail.com', '123456', 'ROLE_CLIENTE'),
-(102, 'bob@gmail.com', '123456', 'ROLE_CLIENTE');
+(100, 'ana@email.com', '123456', 'ROLE_ADMIN'),
+(101, 'bia@email.com', '123456', 'ROLE_CLIENTE'),
+(102, 'bob@email.com', '123456', 'ROLE_CLIENTE');
