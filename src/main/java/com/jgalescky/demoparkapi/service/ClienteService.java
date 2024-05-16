93e19ff3b4399cc67f33a7f9eef5e0c4a67a02ee
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-16 11:48:02 -0300

    feat: Recurso para localizar um cliente pelo identificador

--- src/main/java/com/jgalescky/demoparkapi/service/ClienteService.java
+++ src/main/java/com/jgalescky/demoparkapi/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.jgalescky.demoparkapi.service;
 import com.jgalescky.demoparkapi.entity.Cliente;
 import com.jgalescky.demoparkapi.exception.CpfUniqueViolationException;
 import com.jgalescky.demoparkapi.repository.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,10 @@ public class ClienteService {
         } catch (DataIntegrityViolationException ex) {
             throw new CpfUniqueViolationException(String.format("CPF (%s) não pode ser cadastrado, já existe no sistema", cliente.getCpf()));
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.format("Cliente id=%s não encontrado no sistema", id)));
     }
 }
