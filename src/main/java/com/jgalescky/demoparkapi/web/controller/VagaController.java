ffe631ecc13916a4de05140e9684b32301ef870a
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-17 14:34:43 -0300

    test: Testando o recurso para inserir uma vaga com status 200

--- src/main/java/com/jgalescky/demoparkapi/web/controller/VagaController.java
+++ src/main/java/com/jgalescky/demoparkapi/web/controller/VagaController.java
@@ -5,8 +5,16 @@ import com.jgalescky.demoparkapi.service.VagaService;
 import com.jgalescky.demoparkapi.web.dto.VagaCreateDto;
 import com.jgalescky.demoparkapi.web.dto.VagaResponseDto;
 import com.jgalescky.demoparkapi.web.dto.mapper.VagaMapper;
+import com.jgalescky.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +22,30 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Tag(name = "Vagas", description = "Contém todas as operações relativas ao recurso de uma vaga")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/vagas")
 public class VagaController {
+
     private final VagaService vagaService;
+
+    @Operation(summary = "Criar uma nova vaga", description = "Recurso para criar uma nova vaga"
+            + "Requisição exige um Bearer Token. Acesso restrito a Role='ADMIN'",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso!",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    ),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            }
+    )
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -28,6 +55,20 @@ public class VagaController {
         URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(vaga.getCodigo()).toUri();
         return ResponseEntity.created(location).build();
     }
+
+    @Operation(summary = "Localizar uma vaga", description = "Recurso para retornar uma vaga pelo seu código"
+            + "Requisição exige um Bearer Token. Acesso restrito a Role='ADMIN'",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso criado com sucesso!",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = VagaResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Vaga não localizada",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            }
+    )
 
     @GetMapping("/{codigo}")
     @PreAuthorize("hasRole('ADMIN')")
---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO USUARIOS (id, username, password, role) VALUES
+(100, 'ana@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_ADMIN'),
+(101, 'bia@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(102, 'bob@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(103, 'toby@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(104, 'maguire@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(105, 'carlos@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(106, 'fernando@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(107, 'maria@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
+(108, 'robson@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE');
+
+INSERT INTO VAGAS (id, codigo, status) VALUES
+(10, 'A-01', 'LIVRE'),
+(20, 'A-02', 'LIVRE'),
+(30, 'A-03', 'OCUPADA'),
+(40, 'A-04', 'LIVRE');
