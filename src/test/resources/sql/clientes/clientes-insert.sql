07cde49196cc422110cf7560b9259107d27a2b42
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-17 14:39:29 -0300

    Bugfix: renomeação da tabela CLIENTE para CLIENTES para execução do insert

--- src/test/resources/sql/clientes/clientes-insert.sql
+++ src/test/resources/sql/clientes/clientes-insert.sql
@@ -9,7 +9,7 @@ INSERT INTO USUARIOS (id, username, password, role) VALUES
 (107, 'maria@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE'),
 (108, 'robson@email.com', '$2a$12$wr40UKhti9831Ue016ctrOFwNZ3wTLSU5OO5/gGmQu3Lpmmp0EIJ6', 'ROLE_CLIENTE');
 
-INSERT INTO CLIENTE (id, nome, cpf, id_usuario) VALUES
+INSERT INTO CLIENTES (id, nome, cpf, id_usuario) VALUES
 (10, 'Bianca Silva', '00891800034', 101),
 (20, 'Toby Maguire', '40663565081', 104),
 (30, 'Carlos da Rosa', '22345784008', 105),
